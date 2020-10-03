aabc2506b277abca68b4d0ec3cb5ae5cb809f703
Peter Donald <peter@realityforge.org>
2020-10-03 15:12:18 +1000

    Use a lower delta

--- src/test/java/org/realityforge/vecmath/VecmathAssert.java
+++ src/test/java/org/realityforge/vecmath/VecmathAssert.java
@@ -11,8 +11,8 @@ public abstract class VecmathAssert
 
   public static void assertEquals( @Nonnull final Vec3f vec3f, final float x, final float y, final float z )
   {
-    Assert.assertEquals( vec3f.x, x, 0.001 );
-    Assert.assertEquals( vec3f.y, y, 0.001 );
-    Assert.assertEquals( vec3f.z, z, 0.001 );
+    Assert.assertEquals( vec3f.x, x, 0.00001 );
+    Assert.assertEquals( vec3f.y, y, 0.00001 );
+    Assert.assertEquals( vec3f.z, z, 0.00001 );
   }
 }
