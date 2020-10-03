fe9bc8aae8183b242d54e2f6592d6ff86d9dbdad
Peter Donald <peter@realityforge.org>
2020-10-03 15:33:18 +1000

    Add package docs

---
+++ src/main/java/org/realityforge/vecmath/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * A vector math library.
+ *
+ * <p>The library is developed to be optimized for use in Web applications and uses a RHS coordinate system.</p>
+ */
+package org.realityforge.vecmath;
