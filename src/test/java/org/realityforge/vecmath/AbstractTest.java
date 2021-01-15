76cbc402541f95eac5033b7c12d46739c373cffa
Peter Donald <peter@realityforge.org>
2021-01-15 23:03:04 +1100

    Name components assuming named parameters is column major

--- src/test/java/org/realityforge/vecmath/AbstractTest.java
+++ src/test/java/org/realityforge/vecmath/AbstractTest.java
@@ -101,10 +101,10 @@ abstract class AbstractTest
   }
 
   void assertMatEquals( @Nonnull final Matrix4d actual,
-                        final double m00, final double m01, final double m02, final double m03,
-                        final double m10, final double m11, final double m12, final double m13,
-                        final double m20, final double m21, final double m22, final double m23,
-                        final double m30, final double m31, final double m32, final double m33 )
+                        final double m00, final double m10, final double m20, final double m30,
+                        final double m01, final double m11, final double m21, final double m31,
+                        final double m02, final double m12, final double m22, final double m32,
+                        final double m03, final double m13, final double m23, final double m33 )
   {
     final Matrix4d expected =
       new Matrix4d( m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31, m32, m33 );
